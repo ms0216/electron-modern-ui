0c156e13963d6dfd72fd5fadd764ba11fe0fed1b
ms0216 <miyake.s.0216@gmail.com>
2025-02-08 13:54:32 +0900

    CI/CD: メインウィンドウのサイズを変更し、デベロッパーツールを自動で開くように設定

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ const path = require('path');
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
+
+  mainWindow.webContents.openDevTools();
 
   mainWindow.loadFile('index.html');
 }
