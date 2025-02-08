65b2d32dcf41be676a3c13180cbf98694d84c877
ms0216 <miyake.s.0216@gmail.com>
2025-02-08 11:15:10 +0900

    CI/CD: AvatarComponent にランダムな背景色を追加

--- src/components/data_display/Avatar.js
+++ src/components/data_display/Avatar.js
@@ -5,9 +5,10 @@ import BaseComponent from "../BaseComponent";
 class AvatarComponent extends BaseComponent {
   createAvatar(alt, src) {
     const id = this.generateId();
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
     return {
       id,
-      element: <Avatar id={id} alt={alt} src={src} />
+      element: <Avatar id={id} alt={alt} src={src} style={{ backgroundColor: randomColor }} />
     };
   }
 }
