45df1a598dc58d935b4917c0e58326dc54ee9429
ms0216 <miyake.s.0216@gmail.com>
2025-02-09 12:34:27 +0900

    CI/CD: Autocompleteコンポーネントのcreateメソッドを更新し、デフォルトプロパティを追加

--- src/components/inputs/Autocomplete.js
+++ src/components/inputs/Autocomplete.js
@@ -2,14 +2,21 @@ import React from "react";
 import { Autocomplete, TextField } from "@mui/material";
 import BaseComponent from "../BaseComponent";
 
+const getDefaultProps = (label, options) => ({
+  options: options,
+  getOptionLabel: (option) => option.label || option,
+  renderInput: (params) => <TextField {...params} label={label} variant="outlined" fullWidth />,
+});
+
 class AutocompleteComponent extends BaseComponent {
-  create(label, options = []) {
+  create(label, options = [], props = {}) {
     const id = this.generateId();
+    const defaultProps = getDefaultProps(label, options);
     const element = (
       <Autocomplete
+        {...defaultProps}
         id={id}
-        options={options}
-        renderInput={(params) => <TextField {...params} label={label} variant="outlined" fullWidth />}
+        {...props}
       />
     );
     return { id, element };
